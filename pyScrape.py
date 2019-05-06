03a24a4ff42403f798247abdbad834ad859394e0
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-05-06 13:55:24 -0700

    big data or some shit

--- pyScrape.py
+++ pyScrape.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 article = 'NG SHIT'
 blamNum = 0
 deleteNum = 0
-for num in range(730900, 730905):
+totalLoops = 0
+for num in range(725000, 730905):
+    totalLoops += 1
     url = "https://www.newgrounds.com/portal/view/" + num.__str__()
     try:
         page = urlopen(url)
@@ -16,7 +18,7 @@ for num in range(730900, 730905):
 
     for i in content.findAll('h2'):
         if i.text == "Author Comments":
-            article = article + "\n" + "BLAMMED"
+            article = article + "-------\nBLAMMED\n-------"
             blamNum += 1
             print("BLAMMED")
         else:
@@ -24,9 +26,31 @@ for num in range(730900, 730905):
             print(i.text + ' https://www.newgrounds.com/portal/view/' + num.__str__())
 
             stats = soup.find('div', {"id":"sidestats"})
-            for daStats in stats.findAll('dd'):
-                print(daStats.text)
+
+            if stats.__str__() == "None":
+                print("SOMETHIN BUSTED, SKIPPED")
+                continue
+            else:
+                someVotes = soup.find('span', {"id":"score_number"})
+                if (someVotes.__str__() == "None"):
+                    article = article +'\nVOTES: UNDER JUDGEMENT???'
+                else:
+                    article = article + "\nVOTES: " + someVotes.text + "\n"
+                
+                someTags = soup.find('dd', {"class":"tags momag"})
+                if someTags.__str__() == "None":
+                    print("NO TAGS")
+                    article = article + '\nTAGS: NONE'
+                else:
+                    article = article + '\nTAGS: '
+                    for tags in someTags.findAll('li'):
+                        article = article + tags.text + " "
+                article = article + '\n\n'
+
+            ##for daStats in stats.findAll('dd'):
+            ##    article = article + " " + daStats.text
+            
 else:
-    article = article + "\nBLAMS: " + blamNum.__str__() + "\nDELETES: " + deleteNum.__str__()
+    article = article + "\nBLAMS: " + blamNum.__str__() + "\nDELETES: " + deleteNum.__str__() + "\nTOTAL SUBMISSIONS SCRAPED: " + totalLoops.__str__()
     with open('scraped_text.txt', 'w', encoding='utf-8') as file:
         file.write(article)
